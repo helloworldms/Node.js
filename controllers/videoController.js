73e750c39f33f65e7b312c6cfcd9f8c283d0da38
helloworldms <72796025+helloworldms@users.noreply.github.com>
2021-02-01 23:32:50 +0900

    webpack fail;

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -10,11 +10,20 @@ export const home = async (req, res) => {
     res.render("home", { pageTitle: "Home", videos: [] });
   }
 };
+// Search
 
-export const search = (req, res) => {
+export const search = async (req, res) => {
   const {
     query: { term: searchingBy },
   } = req;
+  let videos = [];
+  try {
+    videos = await Video.find({
+      title: { $regex: searchingBy, $options: "i" },
+    });
+  } catch (error) {
+    console.log(error);
+  }
   res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 
